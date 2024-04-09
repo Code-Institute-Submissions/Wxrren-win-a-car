5931089976fc2eba1c23dcfbde420f1fb6f5b352
Warren smith <warren.lee.smith@hotmail.co.uk>
2024-04-09 11:23:26 +0100

    ADD: Hover Function

---
+++ assets/js/hover.js
@@ -0,0 +1,9 @@
+function logoHover() {
+    document.getElementById("moon-toggle").style.height ="70px"
+    document.getElementById("moon-toggle").style.width ="70px"
+}
+
+function logoNormal() {
+    document.getElementById("moon-toggle").style.height =""
+    document.getElementById("moon-toggle").style.width =""
+}
