81365516ffeeda7049d9d658506b8b761567cda5
Warren smith <warren.lee.smith@hotmail.co.uk>
2024-04-09 18:39:07 +0100

    ADD: hover function for buttons

--- assets/js/hover.js
+++ assets/js/hover.js
@@ -7,3 +7,23 @@ function logoNormal() {
     document.getElementById("moon-toggle").style.height =""
     document.getElementById("moon-toggle").style.width =""
 }
+
+function rulesHover() {
+    document.getElementById("rules-button").style.fontSize = "150%"
+    document.getElementById("rules-button").style.backgroundColor = "red";
+}   
+
+function rulesNormal() {
+    document.getElementById("rules-button").style.fontSize = ""
+    document.getElementById("rules-button").style.backgroundColor = "";
+}  
+
+function contactHover() {
+    document.getElementById("contact-button").style.fontSize = "150%"
+    document.getElementById("contact-button").style.backgroundColor = "red";
+}   
+
+function contactNormal() {
+    document.getElementById("contact-button").style.fontSize = ""
+    document.getElementById("contact-button").style.backgroundColor = "";
+}  
