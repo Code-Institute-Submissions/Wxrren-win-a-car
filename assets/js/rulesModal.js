baebf6a5043661433538454786b3384bd53f7fe3
Wxrren <warren.lee.smith@hotmail.co.uk>
2024-04-11 07:54:25 +0100

    ADD: Rules modal popup

---
+++ assets/js/rulesModal.js
@@ -0,0 +1,9 @@
+let openRules = document.getElementById("rules-modal")
+
+function rulesModal() {
+    openRules.classList.add('rules-active');
+}
+
+function closeRulesModal() {
+    openRules.classList.remove('rules-active');
+}
