472403617fca3b78d8ae04548b52d628258616b2
Wxrren <warren.lee.smith@hotmail.co.uk>
2024-04-12 13:14:58 +0100

    ADD: pseudo code for card game

--- assets/js/cardGame.js
+++ assets/js/cardGame.js
@@ -1,4 +1,6 @@
-const gameContainer = document.getElementsByClassName('card-game-container');
-let cards = [];
-let firstCard, secondCard, thirdCard;
-let lockboard = false;
+// array of strings with filed paths
+// function that will generate a random number
+// function that click binds to card to flip the card
+// function that tracks if the cards display match. Turn/attempt ends when 3 cards are selected.
+// function that pops up saying winner if 3 matches are made - generates an email confirmation with an emial API
+// function that tracks how many attempts are made. Game over when you reach 5. 
