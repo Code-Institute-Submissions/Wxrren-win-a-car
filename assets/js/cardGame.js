70584f7218387e40aa297f11e62eab6cc8f4baea
Wxrren <warren.lee.smith@hotmail.co.uk>
2024-04-12 16:43:56 +0100

    ADD: Array of images for each card when clicked

--- assets/js/cardGame.js
+++ assets/js/cardGame.js
@@ -1,3 +1,32 @@
+let container1 = document.getElementById('card-1');
+let container2 = document.getElementById('card-2');
+let container3 = document.getElementById('card-3');
+let container4 = document.getElementById('card-4');
+let container5 = document.getElementById('card-5');
+let container6 = document.getElementById('card-6');
+let container7 = document.getElementById('card-7');
+let container8 = document.getElementById('card-8');
+let container9 = document.getElementById('card-9');
+
+let contaierArray = [container1, container2, container3, container4, container5, container6, container7, container8, container9];
+let images = [
+    "assets/images/apple.webp", "assets/images/blueberry.webp", "assets/images/car-winner.webp", "assets/images/chilli.webp", "assets/images/corn.webp", "assets/images/grape.webp", "assets/images/orange.webp", "assets/images/stawberry.webp", "assets/images/watermelon.webp"
+];
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+} // The maximum is inclusive and the minimum is inclusive
+
+containerArray.forEach((element) => {
+    element.addEventListener('click', function () {
+        element.src = images[getRandomIntInclusive(0, images.length)];
+    })
+})
+
+
+// function to start game
 // array of strings with filed paths
 // function that will generate a random number
 // function that click binds to card to flip the card
