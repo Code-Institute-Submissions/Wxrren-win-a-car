0ecb9c4f79af545d37071d3d4e9e44d1ec53b708
Wxrren <warren.lee.smith@hotmail.co.uk>
2024-04-17 14:01:16 +0100

    ADD: Message to highlight the darkmode/lightmode toggle

--- assets/js/darkMode.js
+++ assets/js/darkMode.js
@@ -1,5 +1,6 @@
 let darkMode = document.getElementsByTagName("body")[0];
 let lightMode = document.getElementById("moon-toggle");
+let toggleMessage = document.getElementById("toggle-aware")
 
 
 lightMode.addEventListener('click', function() {
@@ -7,7 +8,9 @@ lightMode.addEventListener('click', function() {
     darkMode.classList.toggle("night-theme");
     if(document.body.classList == "night-theme") {
         lightMode.src = "assets/images/sun-logo.webp";
+        toggleMessage.innerHTML = "Light mode"
     } else {
         lightMode.src = "assets/images/moon-logo.webp";
+        toggleMessage.innerHTML = "Dark Mode"
     }
 })
