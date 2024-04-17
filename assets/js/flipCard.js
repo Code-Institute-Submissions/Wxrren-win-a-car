fa37f17c89f3a369f6930ce0059367a76c5c119b
Wxrren <warren.lee.smith@hotmail.co.uk>
2024-04-17 09:34:36 +0100

    ADD: Card flip function

---
+++ assets/js/flipCard.js
@@ -0,0 +1,13 @@
+let container1 = document.getElementById('card-1');
+let container2 = document.getElementById('card-2');
+let container3 = document.getElementById('card-3');
+let container4 = document.getElementById('card-4');
+let container5 = document.getElementById('card-5');
+let container6 = document.getElementById('card-6');
+let container7 = document.getElementById('card-7');
+let container8 = document.getElementById('card-8');
+let container9 = document.getElementById('card-9');
+
+function flipCard1() {
+    container1.classList.add('flip-card');
+}
