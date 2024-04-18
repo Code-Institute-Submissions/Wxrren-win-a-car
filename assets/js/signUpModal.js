0ae67a48ce958e8b716180480d551abfba37c6a0
Wxrren <warren.lee.smith@hotmail.co.uk>
2024-04-18 13:31:58 +0100

    REMOVE: signup button and modal

--- assets/js/signUpModal.js
+++
@@ -1,9 +0,0 @@
-let signUp = document.getElementById("sign-up-modal")
-
-function signUpModal() {
-    signUp.classList.add('sign-up-active');
-}
-
-function closeSignUpModal() {
-    signUp.classList.remove('sign-up-active');
-}
